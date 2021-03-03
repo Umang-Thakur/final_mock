186487955c88804c55b484027095631b5074b819
Umang-Thakur <61343294+Umang-Thakur@users.noreply.github.com>
2021-03-04 00:07:52 +0530

    Initial Commit

--- cet/wsgi.py
+++ cet/wsgi.py
@@ -8,5 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+application = get_wsgi_application()
