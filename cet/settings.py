5cff912a67ab2b03d1f446ab4a19148a3d28295f
Umang-Thakur <61343294+Umang-Thakur@users.noreply.github.com>
2021-03-03 23:34:35 +0530

    Initial Commit

--- cet/settings.py
+++ cet/settings.py
@@ -154,21 +154,21 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   os.path.join(PROJECT_ROOT, "static"),
 ]
 
 # django_heroku.settings(locals())
